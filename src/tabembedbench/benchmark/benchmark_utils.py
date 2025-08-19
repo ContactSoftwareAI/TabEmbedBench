4788bb38e0aaf1ffce098691d45374659d1e121a
Lars Kleinemeier <lars.kleinemeier@contact-software.com>
2025-08-19 10:36:47 +0200

    Add support for multiple models in outlier benchmarking
    - Updated `run_outlier_benchmark` to support a list of models.
    - Added validation to ensure either a single model or a list of models is provided.
    - Enhanced saving embeddings with model-specific directories.
    - Improved metric calculations and result structure for clarity.

--- src/tabembedbench/benchmark/benchmark_utils.py
+++ src/tabembedbench/benchmark/benchmark_utils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pathlib import Path
-from typing import Optional, Union
+from typing import Optional, Union, List
 
 import numpy as np
 import polars as pl
@@ -31,7 +31,8 @@ IMAGE_CATEGORY = [
 
 
 def run_outlier_benchmark(
-    model: BaseEmbeddingGenerator,
+    model: Optional[BaseEmbeddingGenerator] = None,
+    models: Optional[List[BaseEmbeddingGenerator]] = None,
     dataset_paths: Optional[Union[str, Path]] = None,
     save_embeddings: bool = False,
     save_embeddings_path: Optional[Union[str, Path]] = None,
@@ -46,6 +47,7 @@ def run_outlier_benchmark(
     Args:
         model: A model implementing the BaseEmbeddingGenerator interface. This model is used
             for outlier detection across the datasets.
+        models: A list of models implementing the BaseEmbeddingGenerator interface.
         dataset_paths: Path to the directory containing dataset files in `.npz` format. If no
             path is provided, a default path to ADBench tabular datasets is used. The datasets
             will be downloaded if they do not exist at the default location.
@@ -60,6 +62,22 @@ def run_outlier_benchmark(
     Returns:
         pl.DataFrame: A DataFrame containing the combined benchmark results for all datasets.
     """
+    if model is None and models is None:
+        raise ValueError("Either model or models must be provided.")
+    if model is not None and models is not None:
+        raise ValueError("Only one of the parameters 'model' or 'models' should be provided, not both.")
+
+    if model is not None:
+        models_to_process = [model]
+    else:
+        models_to_process = models
+
+    for model_element in models_to_process:
+        if not hasattr(model_element, "compute_embeddings"):
+            raise ValueError(
+                "There is an element within the list of models that does not have a function 'compute_embeddings'."
+            )
+
     logger.info("Running outlier benchmark...")
     if dataset_paths is None:
         dataset_paths = Path("data/adbench_tabular_datasets")
@@ -104,27 +122,34 @@ def run_outlier_benchmark(
         )
         dataset_df = pl.DataFrame(dataset_description)
 
-        result_df = run_experiment(
-            model,
-            X=X,
-            y=y,
-            save_embeddings=save_embeddings,
-            save_embeddings_path=save_embeddings_path,
-            dataset_name=dataset_file.stem,
-        )
-
-        dataset_df = dataset_df.join(result_df, how="cross")
-
-        if benchmark_result_df is None:
-            benchmark_result_df = dataset_df
-        else:
-            benchmark_result_df = _align_schemas_and_concat(
-                benchmark_result_df, dataset_df
+        for model in models_to_process:
+            result_df = run_experiment(
+                model,
+                X=X,
+                y=y,
+                save_embeddings=save_embeddings,
+                save_embeddings_path=save_embeddings_path,
+                dataset_name=dataset_file.stem,
             )
 
-        if benchmark_result_df is None:
-            raise ValueError("Benchmark result DataFrame is empty.")
-        logger.info("Number of rows added: ", benchmark_result_df.height)
+            dataset_df = dataset_df.join(result_df, how="cross")
+
+            dataset_df = (
+                dataset_df.with_columns(
+                    pl.lit(model.name).alias("embedding_model")
+                )
+            )
+
+            if benchmark_result_df is None:
+                benchmark_result_df = dataset_df
+            else:
+                benchmark_result_df = _align_schemas_and_concat(
+                    benchmark_result_df, dataset_df
+                )
+
+            if benchmark_result_df is None:
+                raise ValueError("Benchmark result DataFrame is empty.")
+            logger.info("Number of rows added: ", benchmark_result_df.height)
 
     return benchmark_result_df
 
@@ -144,7 +169,8 @@ def run_experiment(
     X_embed = model.compute_embeddings(X)
 
     if save_embeddings:
-        save_file_path = Path(save_embeddings_path) / f"{dataset_name}_embeddings.npz"
+        save_file_path = Path(save_embeddings_path) / f"{model.name}" / f"{dataset_name}_embeddings.npz"
+        save_file_path.mkdir(parents=True, exist_ok=True)
         np.savez(file=save_file_path, x=X_embed, y=y)
 
     num_neighbors_list = [i for i in range(1, 15)]
@@ -158,18 +184,27 @@ def run_experiment(
             )
 
             lof.fit(X_embed)
-            y_pred = lof.fit_predict(X_embed)
 
-            score_train = compute_metrics(y, y_pred)
+            neg_decision_scores = (-1)*lof.decision_function(X)
+            neg_outlier_factor = (-1)*lof.negative_outlier_factor_
+
+            score_decision = compute_metrics(y, neg_decision_scores)
+            score_outlier_factor = compute_metrics(y, neg_outlier_factor)
 
             result_dict["algorithm"].append("lof")
             result_dict["neighbors"].append(num_neighbors)
 
-            for key, item in score_train.items():
+            for key, item in score_decision.items():
                 if key in result_dict.keys():
-                    result_dict[key].append(item)
+                    result_dict[key + "_decision"].append(item)
                 else:
-                    result_dict[key] = [item]
+                    result_dict[key + "_decision"] = [item]
+
+            for key, item in score_outlier_factor.items():
+                if key in result_dict.keys():
+                    result_dict[key + "_outlier_factor"].append(item)
+                else:
+                    result_dict[key + "_outlier_factor"] = [item]
 
         result_df = pl.DataFrame(result_dict)
     except Exception as e:
@@ -179,7 +214,7 @@ def run_experiment(
         )
 
         # TODO: better handling of error for different metrics.
-        empty_result = {"algorithm": [], "neighbors": [], "auc_score": []}
+        empty_result = {"algorithm": [], "neighbors": [], "auc_score_decision": [], "auc_score_outlier_factor": []}
 
         result_df = pl.DataFrame(empty_result)
 
