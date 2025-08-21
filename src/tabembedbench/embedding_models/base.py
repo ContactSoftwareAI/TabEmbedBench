7f9eb86de430671899867ca29df746fd9f5e2ec3
Lars Kleinemeier <lars.kleinemeier@contact-software.com>
2025-08-21 13:31:06 +0200

    Introduce `Dummy` embedding generator and abstract `preprocess_data` method
    - Added a `Dummy` embedding generator example class inheriting from `BaseEmbeddingGenerator` with minimal implementation of `preprocess_data` and `compute_embeddings` methods.
    - Introduced `preprocess_data` as an abstract method in `BaseEmbeddingGenerator` to standardize data preprocessing.
    - Updated imports to include `torch` and `Union` for improved compatibility and type annotations.

--- src/tabembedbench/embedding_models/base.py
+++ src/tabembedbench/embedding_models/base.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
+from typing import Union
 
 import numpy as np
+import torch
 
 
 class BaseEmbeddingGenerator(ABC):
@@ -39,6 +41,27 @@ class BaseEmbeddingGenerator(ABC):
         self._name = value
 
     @abstractmethod
+    def preprocess_data(self, X: np.ndarray, train: bool = True) -> np.ndarray:
+        """
+        Preprocesses the input data based on training or inference phase.
+
+        This abstract method is intended to apply necessary preprocessing steps,
+        such as scaling, normalization, or transformations, on the provided
+        input dataset. The processing may differ depending on whether the data
+        is being prepared for training or inference.
+
+        Args:
+            X (np.ndarray): Input data array to be preprocessed.
+            train (bool): Indicates whether the preprocessing is for the
+                training phase (`True`) or inference phase (`False`). Defaults
+                to `True`.
+
+        Returns:
+            np.ndarray: Preprocessed data array.
+        """
+        return X
+
+    @abstractmethod
     def compute_embeddings(
         self,
         X: np.ndarray,
@@ -54,3 +77,29 @@ class BaseEmbeddingGenerator(ABC):
 
         """
         raise NotImplementedError
+
+
+class Dummy(BaseEmbeddingGenerator):
+    """
+    Class representing a dummy embedding generator.
+
+    This class is designed to showcase a basic embedding generator. It includes method
+    stubs for preprocessing data and computing embeddings. It inherits from
+    `BaseEmbeddingGenerator`.
+
+    Attributes:
+        name (str): The name of the embedding generator, set to a default value during initialization.
+    """
+    def __init__(self):
+        self.name = self._get_default_name()
+
+    def _get_default_name(self) -> str:
+        return "Dummy"
+
+    def preprocess_data(self, X: Union[torch.Tensor, np.ndarray], **kwargs) -> np.ndarray:
+        return X
+
+    def compute_embeddings(
+        self, X: Union[torch.Tensor, np.ndarray]
+    ) -> np.ndarray:
+        return np.random.rand(X.shape[0], 10)
