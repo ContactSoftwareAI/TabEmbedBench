9e5314c6e86867edb5f4e342e11d7328ccd2e4e7
Lars Kleinemeier <lars.kleinemeier@contact-software.com>
2025-10-06 15:07:32 +0200

    Add ECODEvalutor and DeepSVDDEvaluator to the benchmark.

--- src/tabembedbench/evaluators/__init__.py
+++ src/tabembedbench/evaluators/__init__.py
@@ -1,1 +1,15 @@
 from tabembedbench.evaluators.abstractevaluator import AbstractEvaluator
+from tabembedbench.evaluators.outlier import (
+    ECODEvaluator,
+    DeepSVDDEvaluator,
+    LocalOutlierFactorEvaluator,
+    IsolationForestEvaluator,
+)
+
+__all__ = [
+    "AbstractEvaluator",
+    "ECODEvaluator",
+    "DeepSVDDEvaluator",
+    "LocalOutlierFactorEvaluator",
+    "IsolationForestEvaluator",
+]
